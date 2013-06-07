882b564d448b1afcb4297056b4727972dbca4a26
Kazu Yamamoto <kazu@iij.ad.jp>
2013-06-07 14:48:19 +0900

    clean up.

--- single/echo-server.hs
+++ single/echo-server.hs
@@ -10,29 +10,30 @@ import qualified Data.ByteString as BS
 import Network.Socket
 import qualified Network.Socket.ByteString as NBS
 
+echo :: Socket -> IO ()
+echo sock = do
+  bs <- NBS.recv sock ECHO_SIZE
+  when (BS.length bs > 0) $ do
+    NBS.send sock bs
+    echo sock
+
+server :: IO ()
+server = do
+    (addr:_) <- getAddrInfo (Just (defaultHints { addrFlags = [AI_PASSIVE] }))
+                            Nothing
+                            (Just ECHO_PORT)
+    listenSock <- socket (addrFamily addr) Stream defaultProtocol
+    setSocketOption listenSock ReuseAddr 1
+    bindSocket listenSock (addrAddress addr)
+
+    listen listenSock MAX_BACKLOG
+    (sock, _) <- accept listenSock
+    setSocketOption sock NoDelay 1
+
+    echo sock
+
 main :: IO ()
 main = do
     putStrLn "Echo server started."
-    serverAddrs <- getAddrInfo
-                   (Just (defaultHints { addrFlags = [AI_PASSIVE] } ))
-                   Nothing
-                   (Just ECHO_PORT)
-    let serverAddr = head serverAddrs
-    sock <- socket (addrFamily serverAddr) Stream defaultProtocol
-    setSocketOption sock ReuseAddr 1
-    setSocketOption sock NoDelay 1
-    bindSocket sock (addrAddress serverAddr)
-
-    listen sock 5
-    (clientSock, _) <- accept sock
-
-    runInUnboundThread $ pong clientSock
+    runInUnboundThread server
     putStrLn "Echo server finished."
-
-pong :: Socket -> IO ()
-pong sock = do
-  bs <- NBS.recv sock 8
-  -- putStrLn $ "server received " ++ unpack bs
-  when (BS.length bs > 0) $ do
-    NBS.send sock bs
-    pong sock
