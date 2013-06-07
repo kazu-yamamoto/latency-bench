359ecf1b6602feaa631d664b9097109a0147abda
Kazu Yamamoto <kazu@iij.ad.jp>
2013-06-07 16:15:17 +0900

    client now uses lib.

--- single/echo-client.c
+++ single/echo-client.c
@@ -13,63 +13,32 @@
 
 #include "echo.h"
 
-double timestamp() {
+double
+timestamp() {
   struct timeval tp;
   gettimeofday(&tp, NULL);
   return ((double) tp.tv_sec) * 1e6 + (double) tp.tv_usec;
 }
 
-int client(char *addr, int pings) {
-  struct addrinfo hints, *res;
-  int error, client_socket, i;
+int
+client(char *peer, int num) {
+  int sock = connect_peer(peer, ECHO_PORT);
+  int i;
 
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family   = AF_INET;
-  hints.ai_socktype = SOCK_STREAM;
-
-  error = getaddrinfo(addr, ECHO_PORT, &hints, &res);
-  if(error) {
-    fprintf(stderr, "client error: %s\n", gai_strerror(error));
-    return -1;
-  }
-
-  client_socket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-  if(client_socket < 0) {
-    fprintf(stderr, "client error: could not create socket\n");
-    return -1;
-  }
-
-  if(connect(client_socket, res->ai_addr, res->ai_addrlen) < 0) {
-    fprintf(stderr, "client error: could not connect: %s\n", strerror(errno));
-    return -1;
-  }
-
-  for(i = 0; i < pings; i++) {
+  for (i = 0; i < num; i++) {
     double timestamp_before = timestamp();
-
-    send(client_socket, "ping123", 8, 0);
-
-    char *buf = malloc(8);
-    ssize_t read = recv(client_socket, buf, 8, 0);
-
-    if(read == 0) {
-      fprintf(stderr, "server exited prematurely\n");
-      free(buf);
-      break;
-    }
-
-    free(buf);
-
+    if (ping(sock) == 0) { break; }
     double timestamp_after = timestamp();
     printf("%i %lf\n", i, timestamp_after - timestamp_before);
   }
 
-  freeaddrinfo(res);
   return 0;
 }
 
-int usage(int argc, char** argv) {
-  fprintf(stderr, "usage: %s <server_addr> <#_of_pings>\n", argv[0]);
+int
+usage(int argc, char** argv) {
+  char *prog = argv[0];
+  fprintf(stderr, "usage: %s <server_addr> <#_of_pings>\n", prog);
   return -1;
 }
 
@@ -78,9 +47,9 @@ main(int argc, char** argv) {
   if(argc != 3) {
     return usage(argc, argv);
   }
-  char *addr = argv[1];
-  char *num = argv[2];
-  int pings = 0;
-  sscanf(num, "%d", &pings);
-  return client(addr, pings);
+  char *peer  = argv[1];
+  char *pings = argv[2];
+  int num = 0;
+  sscanf(pings, "%d", &num);
+  return client(peer, num);
 }
