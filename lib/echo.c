06f4bb4fb269eb090dab62b3dbbbaddc4dc7635d
Kazu Yamamoto <kazu@iij.ad.jp>
2013-06-07 14:22:56 +0900

    clean up lib.

--- lib/echo.c
+++ lib/echo.c
@@ -1,79 +1,121 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 #include "echo.h"
 
+////////////////////////////////////////////////////////////////
+
+void
+exit_error(int ret, char *msg) {
+  if (ret < 0) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+  }
+}
+ 
+void
+exit_gai_error(int ret) {
+  if (ret != 0) {
+    fprintf(stderr, "%s\n", gai_strerror(ret));
+    exit(EXIT_FAILURE);
+  }
+}
+
+////////////////////////////////////////////////////////////////
+
+struct addrinfo *
+get_my_addr(char *port) {
+  struct addrinfo hints, *addr;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_flags = AI_PASSIVE;
+  hints.ai_family = AF_INET6;
+  hints.ai_socktype = SOCK_STREAM;
+  int ret = getaddrinfo(NULL, port, &hints, &addr);
+  exit_gai_error(ret);
+  return addr;
+}
+ 
+int
+get_server_socket(struct addrinfo *addr) {
+  int sock = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+  exit_error(sock, "socket");
+  return sock;
+}
+ 
+int
+get_socket() {
+  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+  exit_error(sock, "socket");
+  return sock;
+}
+
+void
+bind_socket(int sock, struct addrinfo *addr) {
+  int ret = bind(sock, addr->ai_addr, addr->ai_addrlen);
+  exit_error(ret, "bind");
+}
+ 
+void
+listen_socket(int sock, int qlen) {
+  int ret = listen(sock, qlen);
+  exit_error(ret, "socket");
+}
+
+void
+set_reuseaddr(int sock) {
+  int on = 1;
+  int ret = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+  exit_error(ret, "setsockopt REUSEADDR");
+}
+
+void
+set_nonblock(int sock) {
+  int flags = fcntl(sock, F_GETFL);
+  int ret = fcntl(sock, F_SETFL, (flags | O_NONBLOCK));
+  exit_error(ret, "NONBLOCK");
+}
+
 int
 listen_to_echo_port () {
-  struct addrinfo hints, *res;
-  int error, sock;
-
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family   = AF_INET;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags    = AI_PASSIVE;
-
-  error = getaddrinfo(NULL, ECHO_PORT, &hints, &res);
-  if(error) {
-    printf("getaddrinfo(): %s\n", gai_strerror(error));
-    return -1;
-  }
-
-  sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-  if(sock < 0) {
-    printf("socket()\n");
-    return -1;
-  }
-
-  int yes = 1;
-  if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0) {
-    printf("setsockopt()\n");
-    return -1;
-  }
-
-  if(bind(sock, res->ai_addr, res->ai_addrlen) < 0) {
-    printf("bind()\n");
-    return -1;
-  }
-
-  listen(sock, MAX_BACKLOG);
-
-  freeaddrinfo(res);
-
+  struct addrinfo *addr = get_my_addr(ECHO_PORT);
+  int sock = get_server_socket(addr);
+  set_reuseaddr(sock);
+  bind_socket(sock, addr);
+  listen_socket(sock, MAX_BACKLOG);
+  freeaddrinfo(addr);
   return sock;
 }
+
+////////////////////////////////////////////////////////////////
 
 int
-accept_client(int listen_socket) {
-  int sock;
-  struct sockaddr_storage client_addr;
-  socklen_t addr_size;
-
-  sock = accept(listen_socket, (struct sockaddr *)&client_addr, &addr_size);
-  return sock;
+accept_client(int sock) {
+  int ssock= accept(sock, NULL, NULL);
+  exit_error(ssock, "accept");
+  return ssock;
 }
+
+////////////////////////////////////////////////////////////////
 
 int
 echo(int sock) {
   char buf[ECHO_SIZE];
-  size_t len;
 
-  if ((len = recv(sock, buf, ECHO_SIZE, 0)) < 0) {
-    printf("recv()\n");
-    return -1;
-  }
+  size_t rlen = recv(sock, buf, ECHO_SIZE, 0);
+  exit_error(rlen, "recv()");
 
-  if (len == 0) {
+  if (rlen == 0) {
     return 0;
   }
 
-  if (send(sock, buf, len, 0) < 0) {
-    printf("send()\n");
-    return -1;
-  }
+  size_t slen = send(sock, buf, rlen, 0);
+  exit_error(slen, "send()");
 
-  return len;
+  return rlen;
 }
--- lib/echo.h
+++ lib/echo.h
@@ -5,5 +5,7 @@
 extern int listen_to_echo_port ();
 extern int accept_client(int);
 extern int echo(int);
+extern void set_nonblock(int);
+
 
 
