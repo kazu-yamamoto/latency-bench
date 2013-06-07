f5fbcf9ed3d7ee4a4ad32681eea840924b1dda1a
Kazu Yamamoto <kazu@iij.ad.jp>
2013-06-07 13:14:45 +0900

    using echo lib.

--- single/echo-server.c
+++ single/echo-server.c
@@ -1,77 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netdb.h>
-#include <string.h>
-#include <unistd.h>
-#include <signal.h>
-#include <errno.h>
-#include <time.h>
-#include <sys/time.h>
-#include <math.h>
 
 #include "echo.h"
 
 int server() {
-  printf("Echo server started.\n");
-
-  struct addrinfo hints, *res;
-  int error, server_socket;
-
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family   = PF_INET;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags    = AI_PASSIVE;
-
-  error = getaddrinfo(NULL, ECHO_PORT, &hints, &res);
-  if(error) {
-    printf("server error: %s\n", gai_strerror(error));
-    return -1;
-  }
-
-  server_socket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-  if(server_socket < 0) {
-    printf("server error: could not create socket\n");
-    return -1;
-  }
-
-  int yes = 1;
-  if(setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0) {
-    printf("server error: could not set socket options\n");
-    return -1;
-  }
-
-  if(bind(server_socket, res->ai_addr, res->ai_addrlen) < 0) {
-    printf("server error: could not bind to socket\n");
-    return -1;
-  }
-
-  listen(server_socket, 5);
-
-  int client_socket;
-  struct sockaddr_storage client_addr;
-  socklen_t addr_size;
-  client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_size);
+  int listen_socket = listen_to_echo_port();
+  int sock = accept_client(listen_socket);
 
   for(;;) {
     char* buf = malloc(8);
-    ssize_t read = recv(client_socket, buf, 8, 0);
+    ssize_t read = recv(sock, buf, 8, 0);
     if(read == 0) {
       free(buf);
       break;
     }
-    // printf("server received '%s'\n", buf);
-    send(client_socket, buf, 8, 0);
+    send(sock, buf, 8, 0);
     free(buf);
   }
 
-  printf("Echo server finished.\n");
-  freeaddrinfo(res);
   return 0;
 }
 
 int
 main(int argc, char** argv) {
-  return server();
+  printf("Echo server started.\n");
+  server();
+  printf("Echo server finished.\n");
+  return 0;
 }
