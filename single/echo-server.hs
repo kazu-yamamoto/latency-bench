cda242de2f4039468ea84e9c9be73b6ef434d1ec
Kazu Yamamoto <kazu@iij.ad.jp>
2013-06-07 14:59:56 +0900

    using void.

--- single/echo-server.hs
+++ single/echo-server.hs
@@ -14,7 +14,7 @@ echo :: Socket -> IO ()
 echo sock = do
   bs <- NBS.recv sock ECHO_SIZE
   when (BS.length bs > 0) $ do
-    NBS.send sock bs
+    void $ NBS.send sock bs
     echo sock
 
 server :: IO ()
@@ -25,7 +25,6 @@ server = do
     listenSock <- socket (addrFamily addr) Stream defaultProtocol
     setSocketOption listenSock ReuseAddr 1
     bindSocket listenSock (addrAddress addr)
-
     listen listenSock MAX_BACKLOG
     (sock, _) <- accept listenSock
     setSocketOption sock NoDelay 1
